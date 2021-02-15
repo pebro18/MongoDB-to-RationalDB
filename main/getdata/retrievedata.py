588a42fbd42aef33082f39e423bc2565f1157485
Debug-Sam <74404094+Debug-Sam@users.noreply.github.com>
2021-02-15 15:31:36 +0100

    mappen structuur aangemaakt

---
+++ main/getdata/retrievedata.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+all_col = ['products', 'sessions', 'visitors']
+myclient = MongoClient("mongodb://localhost:27017/")
+mydb = myclient["huwebshop"]
+mycol = mydb[all_col[0]]
+
+for i in mycol.find({'brand': "8x4"}, {"_id": 0, "brand": 1}):
+    print(i)
+
+
+class getdata(object):
+
+
+    def __init__(self):
+        global all_col
+        all_col = ['products', 'sessions', 'visitors']
+        global myclient
+        myclient = MongoClient("mongodb://localhost:27017/")
+        global mydb
+        mydb = myclient["huwebshop"]
+        global mycol
+        mycol = mydb[all_col[0]]
+
+
+    def first_name_price(self):
+        return mycol.find_one({},{"_id": 0, "name": 1, "price.selling_price": 1})
+
+    def name_startR(self):
+        return
+
+
+
